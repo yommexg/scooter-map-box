e051f1766e30b791e597800fd1879e2b990ca0f8
yommexg <hydrokidb@gmail.com>
2024-12-21 04:35:32 +0000

    Renders Markers on the Map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,23 @@
-import MapboxGL, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
+import MapboxGL, {
+  Camera,
+  Images,
+  LocationPuck,
+  MapView,
+  ShapeSource,
+  SymbolLayer,
+} from '@rnmapbox/maps';
 import React, { useEffect, useState } from 'react';
 import { View, Text, Platform, StyleSheet } from 'react-native';
 import * as Location from 'expo-location';
+import { featureCollection, point } from '@turf/helpers';
+import pin from '~/assets/pin.png';
+import scooters from '~/data/scooter.json';
 
 MapboxGL.setAccessToken(process.env.EXPO_PUBLIC_MAP_ACCESS_PUBLIC_TOKEN!);
 
 export default function Map() {
+  const points = scooters.map((scooter) => point([scooter.long, scooter.lat]));
+
   useEffect(() => {
     const requestLocationPermission = async () => {
       const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
@@ -30,6 +42,17 @@ export default function Map() {
     <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
       <Camera followUserLocation followZoomLevel={10} />
       <LocationPuck puckBearing="course" puckBearingEnabled={true} pulsing={{ isEnabled: true }} />
+      <ShapeSource id="scooters" shape={featureCollection(points)}>
+        <SymbolLayer
+          id="scooter-icons"
+          style={{
+            iconImage: 'pin',
+            iconSize: 0.4,
+            iconAllowOverlap: true,
+          }}
+        />
+        <Images images={{ pin }} />
+      </ShapeSource>
     </MapView>
   );
 }
